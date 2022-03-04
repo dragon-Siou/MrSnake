be4b9e074e1bb0045d6c5dc55459182be4bc6654
dragon-Siou <72135945+dragon-Siou@users.noreply.github.com>
2022-03-04 22:20:36 +0800

    ChangeTurnRTL

--- js/magazine.js
+++ js/magazine.js
@@ -206,14 +206,14 @@ function isChrome() {
 
 function disableControls(page) {
 		if (page==1)
-			$('.previous-button').hide();
-		else
-			$('.previous-button').show();
-					
-		if (page==$('.magazine').turn('pages'))
 			$('.next-button').hide();
 		else
 			$('.next-button').show();
+					
+		if (page==$('.magazine').turn('pages'))
+			$('.previous-button').hide();
+		else
+			$('.previous-button').show();
 }
 
 // Set the width and height for the viewport
