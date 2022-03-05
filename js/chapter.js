7e2be2f171e39a9713c0e81d44ad7acd573e729b
dragon-Siou <72135945+dragon-Siou@users.noreply.github.com>
2022-03-06 00:27:25 +0800

    AddChapter

---
+++ js/chapter.js
@@ -0,0 +1,24 @@
+$(function(){
+    //設定第一章按鈕
+    $(".chapter-button-head").click(function(){
+        $('.magazine').turn("page",1)
+    })
+    $(".chapter-button-0").click(function(){
+        $('.magazine').turn("page",2)
+    })
+    $(".chapter-button-1").click(function(){
+        $('.magazine').turn("page",26)
+    })
+    $(".chapter-button-2").click(function(){
+        $('.magazine').turn("page",47)
+    })
+    $(".chapter-button-3").click(function(){
+        $('.magazine').turn("page",68)
+    })
+    $(".chapter-button-4").click(function(){
+        $('.magazine').turn("page",89)
+    })
+    $(".chapter-button-5").click(function(){
+        $('.magazine').turn("page",113)
+    })
+})
