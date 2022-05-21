c812fc5781c74a2fd2980e2d974032ff86e30345
dragon-Siou <72135945+dragon-Siou@users.noreply.github.com>
2022-05-21 23:49:53 +0800

    update_0521

--- js/background.js
+++ js/background.js
@@ -23,6 +23,9 @@ $(function(){
 
             //原本是大的，回到正常大小
             $('.magazine-viewport').addClass("normal").removeClass("big");
+
+            //開啟所有物件
+            closeAllOptions();
         }
         else{
 
@@ -34,6 +37,9 @@ $(function(){
             //$('.magazine').turn("size", h.heightNum*1.5, h*1.5);
             //原本是小的，放大
             $('.magazine-viewport').addClass("big").removeClass("normal");
+
+            //關掉所有物件
+            openAllOptions();
         }
 
         
@@ -53,6 +59,14 @@ function backToComic(){
 
     //開啟漫畫
     $(".magazine").css("display","block");
+}
+
+function closeMagnifier(){
+    $(".magnifier").css("display","none");
+}
+
+function openMagnifier(){
+    $(".magnifier").css("display","block");
 }
 
 function closeAllOptions(){
@@ -80,8 +94,9 @@ function setMenu(){
         //$(".magazine-viewport").unbind("click", menuBackToComic)
 
         //重製動畫
+       
         $("#menu-lightbox").removeClass("lightbox-menu-open")
-            //lightbox動畫
+           //lightbox動畫
             .addClass("lightbox-menu-close")
 
         $("#menu-lightbox").removeClass("lightbox-target")//.addClass("lightbox-hide");
@@ -94,7 +109,8 @@ function setMenu(){
         //把桌面上的隱藏
         //$(".menu").addClass("menu-close").removeClass("menu-open")
         closeAllOptions();
-
+        //開放大鏡
+        openMagnifier()
     }
 
     $("#menu-close").click(menuBackToComic)
@@ -112,10 +128,12 @@ function setMenu(){
         //把桌面上的開起來
         openAllOptions();
         //動畫
-        /*
-        $(this).removeClass('fadeIn');*/
+        
+        $(this).removeClass('fadeIn');
+
         $("#menu-lightbox").addClass("lightbox-menu-open")
         .removeClass("lightbox-hide")//.addClass("lightbox-target")
+   
             //.addClass("animate__fadeInTopLeft animate__animated ")
             //.removeClass("animate__fadeOutTopLeft")
             .removeClass("lightbox-menu-close")
@@ -126,8 +144,8 @@ function setMenu(){
 
         canFlip=false;
 
-        
-
+        //關放大鏡
+        closeMagnifier()
         //$(".magazine-viewport").click(menuBackToComic)
         return false;
     })
@@ -136,17 +154,34 @@ function setMenu(){
 
     //選單按鈕設定
     let menuButtonIndex={
-        0:2,
-        1:26,
-        2:47,
-        3:68,
-        4:88,
-        5:112,
-        6:134
+        0:5,
+        1:29,
+        2:51,
+        3:73,
+        4:95,
+        5:120,
+        6:142,
+        7:164
     }
 
+
+    //重製所有頁面
+    let closeAllChapter = function(){
+        for( let key in menuButtonIndex){
+            //變暗
+            $("#episode-" + key).addClass("menu-page-black")
+
+            //重製位移
+            $("#episode-" + key).css("left", "")
+                    .css("z-index" , "")
+        }
+    }
     
     for( let key in menuButtonIndex){
+
+        //預設沒有選擇，所以是黑色
+        $("#episode-" + key).addClass("menu-page-black")
+
         //用ID去取得button
         $("#episode-" + key).click(function(){
             
@@ -160,6 +195,33 @@ function setMenu(){
 
             setTimeout(turn, 100)
         })
+
+        
+
+        let newKey = key;
+
+        $("#episode-" + key).hover(function(){
+            //關閉所有
+            closeAllChapter();
+            //顯示選擇
+            $("#episode-" + newKey).removeClass("menu-page-black")
+
+            //位移
+            for(i=0; i<newKey ;i++){
+                xOffset = (85 - i*10) + 15
+                $("#episode-" + i).css("left", xOffset + "%")
+                    .css("z-index" , 350)
+            }
+
+            /*
+            xPosition = (85 - newKey*10)
+            //自己也要位移
+            $("#episode-" + newKey).css("left", xPosition + "%")*/
+        },
+        //hover結束
+        function(){
+            closeAllChapter();
+        })
     }
 
 
@@ -170,7 +232,7 @@ function setMenu(){
 
 
     //開發中封條
-    showSeal();
+    //showSeal();
     
 }
 
@@ -296,9 +358,122 @@ function setAuthor(){
     })
 }
 
-function setTablet(){
 
+function closeAllTabletContent(){
+    $(".tablet-button-box").css("display", "none")
+    $(".tablet-introduction-box").css("display", "none")
+    $(".tablet-trailer-box").css("display", "none")
 }
+
+function setTablet(){
+     //回到閱讀模式
+     let tabletBackToComic = function(){
+
+        backToComic()
+
+        //重製動畫
+        $("#tablet-lightbox").removeClass("lightbox-tablet-open")
+            //lightbox動畫
+            .addClass("lightbox-tablet-close")
+
+        $("#tablet-lightbox").removeClass("lightbox-target")//.addClass("lightbox-hide");
+
+
+        //.removeClass("animate__fadeInTopLeft animate__animated")
+
+            //離開動畫
+            //.addClass("animate__fadeOutTopLeft");
+        //把桌面上的隱藏
+        //$(".author").addClass("author-close").removeClass("author-open")
+        closeAllOptions();
+
+        //開啟放大鏡
+        openMagnifier();
+    }
+
+
+
+    //點擊作者
+    $(".tablet").click(function(){
+        
+        element = document.querySelector('#tablet-lightbox');
+        element.style.setProperty('--animate-duration', '0.5s');
+
+        //播聲音
+        let music = new Audio("./music/test.mp3");
+        music.play();
+
+        //把桌面上的開起來
+        openAllOptions();
+
+        //動畫
+        /*
+        $(this).removeClass('fadeIn');*/
+        $("#tablet-lightbox").addClass("lightbox-tablet-open")
+        .removeClass("lightbox-hide")//.addClass("lightbox-target")
+            //.addClass("animate__fadeInTopLeft animate__animated ")
+            //.removeClass("animate__fadeOutTopLeft")
+            .removeClass("lightbox-tablet-close")
+
+
+        //關掉漫畫
+        $(".magazine").css("display","none");
+
+        canFlip=false;
+
+        //關掉放大鏡
+        closeMagnifier();
+        
+
+       
+        return false;
+    })
+
+
+    $(".tablet-button-return").click(tabletBackToComic)
+
+    //進入簡介
+    $(".tablet-button-introduction").click(function(){
+        closeAllTabletContent();
+        $(".tablet-introduction-box").css("display", "block")
+    })
+
+    //簡介的返回按鈕
+    $(".tablet-introduction-back").click(function(){
+        closeAllTabletContent();
+        $(".tablet-button-box").css("display", "block")
+    })
+
+
+    //進入預告片
+    $(".tablet-button-trailer").click(function(){
+        closeAllTabletContent();
+        $(".tablet-trailer-box").css("display", "block")
+    })
+
+    //簡介的返回按鈕
+    $(".tablet-trailer-back").click(function(){
+        closeAllTabletContent();
+        $(".tablet-button-box").css("display", "block")
+    })
+
+
+    //初始設定
+    closeAllTabletContent();
+    $(".tablet-button-box").css("display", "block")
+
+    //lightbox的點擊不要冒泡
+    $("#tablet-lightbox").click(function(){
+        return false;
+    })
+}
+
+
+
+
+
+//timeout陣列
+let timeoutID;
 
 function setWhiltboard(){
     //回到閱讀模式
@@ -311,6 +486,7 @@ function setWhiltboard(){
 
         //重製動畫
         $("#whiteboard-lightbox").removeClass("lightbox-whiteboard-open")
+
             //lightbox動畫
             .addClass("lightbox-whiteboard-close")
 
@@ -324,6 +500,11 @@ function setWhiltboard(){
         //把桌面上的隱藏
         //$(".author").addClass("author-close").removeClass("author-open")
         closeAllOptions();
+
+        //關放大鏡
+        openMagnifier();
+
+        resetTV();
     }
 
 
@@ -341,6 +522,10 @@ function setWhiltboard(){
         //把桌面上的開起來
         openAllOptions();
 
+        //關放大鏡
+        closeMagnifier();
+        
+
         //動畫
         /*
         $(this).removeClass('fadeIn');*/
@@ -350,6 +535,15 @@ function setWhiltboard(){
             //.removeClass("animate__fadeOutTopLeft")
             .removeClass("lightbox-whiteboard-close")
 
+        //TV淡入動畫
+       // 
+        setTimeout(
+            function(){
+                $(".whiteboard-TV").addClass("lightbox-TV-open");
+
+            }
+            ,1000
+        )
 
         //關掉漫畫
         $(".magazine").css("display","none");
@@ -358,8 +552,50 @@ function setWhiltboard(){
 
         
 
-        $(".magazine-viewport").click(whiteboardBackToComic)
+        //$(".magazine-viewport").click(whiteboardBackToComic)
         return false;
+    })
+
+    //設定返回按鈕
+    $(".whiteboard-back").click(whiteboardBackToComic)
+
+    //設定電源按鈕
+    $(".whiteboard-switch-button").click(function(){
+
+        //如果第二次按，reset
+        if($("#TV").hasClass("lightbox-TV-switch-on") || $("#TV").hasClass("lightbox-TV-relations")){
+            resetTV();
+            //取消所有callback
+            window.clearTimeout(timeoutID);
+            return;
+        }
+
+        //換背景
+        cleanTVImage()
+        $("#TV").addClass("lightbox-TV-switch-on");
+            
+
+        //文字
+        $("#TV-text")
+            .addClass("lightbox-text-open")
+            .removeClass("whiteboard-text-off");
+
+        //動畫播完，要進入角色關係圖
+        timeoutID = setTimeout(function(){
+            
+
+
+            //關掉文字
+            $("#TV-text")
+                .addClass("whiteboard-text-off")
+                .removeClass("lightbox-text-open");
+
+            //換背景
+            cleanTVImage();
+            $("#TV").addClass("lightbox-TV-relations");
+
+
+        }, 6000);
     })
 
 
@@ -367,4 +603,26 @@ function setWhiltboard(){
     $("#whiteboard-lightbox").click(function(){
         return false;
     })
+}
+
+function cleanTVImage(){
+    $("#TV")
+    .removeClass("lightbox-TV-switch-on")
+    .removeClass("lightbox-TV-switch-off")
+    .removeClass("lightbox-TV-relations")
+}
+
+function resetTV(){
+    //重設TV
+
+    cleanTVImage();
+    $("#TV")
+    .addClass("lightbox-TV-switch-off")
+
+
+    //重設文字
+    //文字
+    $("#TV-text")
+    .addClass("whiteboard-text-off")
+    .removeClass("lightbox-text-open")
 }
